1e2b85973f7bec5dd1ee97174374e3f02d99daa0
Arun Sivakumar <arunsivakumar@live.com>
2018-02-09 16:52:38 +0800

    add ChatVc

--- CirclesBot/ViewController.swift
+++ CirclesBot/ViewController.swift
@@ -7,19 +7,130 @@
 //
 
 import UIKit
+import JSQMessagesViewController
 
-class ViewController: UIViewController {
+class ViewController: JSQMessagesViewController {
 
+// MARK: Public API
+    var chatStore:ChatStore!
+    
+    var messages = [JSQMessage]()
+    
+
+// MARK: Private Variables
+    
+    // Chat Bubble Configuration
+    fileprivate var incomingMessage: JSQMessagesBubbleImage!
+    fileprivate var incomingMessageTextColor = UIColor.black
+    
+    fileprivate var outgoingMessage: JSQMessagesBubbleImage!
+    fileprivate var outgoingMessageTextColor = UIColor.white
+ 
+    
+// MARK: Lifecycle
+    
     override func viewDidLoad() {
+        setupSender() // Super expects sender to be set
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+        setupUI()
+        setupChatUI()
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
+    
+    fileprivate func sendMessage(message:String){
+        chatStore.fetchText(message: message) { [weak self](result) in
+            switch result{
+            case .success(let message):
+                    self?.messages.append(message)
+                    self?.finishReceivingMessage()
+            case .failure( _): break
+                    // show error
+            }
+            
+        }
+    }
+    
+    private func setupSender() {
+        senderId = User.me.rawValue
+        senderDisplayName = User.getName(User.me)
+    }
+    
+    private func setupUI(){
+        self.navigationItem.title = "CirclesBot"
+        self.navigationController?.navigationBar.barTintColor = UIColor(red: 15/255, green: 131/255, blue: 255/255, alpha: 1.0)
+        self.navigationController?.navigationBar.tintColor = UIColor.white
+        self.navigationController!.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName: UIColor.white]
+    }
+    
+    private func setupChatUI() {
+        
+        // messages
+        
+        let incomingMessageColor = UIColor.lightGray
+        let outgoingMessageColor = UIColor.blue
+        incomingMessage = JSQMessagesBubbleImageFactory().incomingMessagesBubbleImage(with: incomingMessageColor)
+        outgoingMessage = JSQMessagesBubbleImageFactory().outgoingMessagesBubbleImage(with: outgoingMessageColor)
+        
+         self.inputToolbar.contentView.leftBarButtonItem = nil
+        
+        // test data
+        //messages.append(JSQMessage( senderId: User.bot.rawValue, senderDisplayName: User.getName(User.me), date: Date(),text: "Hello"))
+       
+        
+    }
 
 }
 
+// MARK: Send Button Delegate
+extension ViewController {
+    
+    override func didPressSend(_ button: UIButton!, withMessageText text: String!, senderId: String!, senderDisplayName: String!, date: Date!) {
+        
+        
+        // append to existing Data structure
+        guard let message = JSQMessage( senderId: User.me.rawValue, senderDisplayName: User.getName(User.me), date: date,text: text)else {
+            return // error checking / alert message
+        }
+        
+        self.messages.append(message)
+        self.finishSendingMessage(animated: true)
+        
+        sendMessage(message: text)
+        
+        // Send Actual data
+        
+    }
+}
+
+// MARK: JSQMessages Data Source
+
+extension ViewController {
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int{
+        return messages.count
+    }
+    
+    override func collectionView(_ collectionView: JSQMessagesCollectionView!, messageDataForItemAt indexPath: IndexPath!)
+        -> JSQMessageData!{
+        return messages[indexPath.item]
+    }
+    
+    override func collectionView(_ collectionView: JSQMessagesCollectionView!, messageBubbleImageDataForItemAt indexPath:IndexPath!) -> JSQMessageBubbleImageDataSource!{
+        let message = messages[indexPath.item]
+        let messageBubble = (message.senderId == senderId) ? outgoingMessage : incomingMessage
+        return messageBubble
+    }
+    
+    override func collectionView( _ collectionView: JSQMessagesCollectionView!,avatarImageDataForItemAt indexPath: IndexPath!)
+        -> JSQMessageAvatarImageDataSource!{
+        let message = messages[indexPath.item]
+        return User.getAvatar(message.senderId)
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView,cellForItemAt indexPath: IndexPath) ->UICollectionViewCell{
+        let cell = super.collectionView( collectionView, cellForItemAt: indexPath) as! JSQMessagesCollectionViewCell
+        let message = messages[indexPath.item]
+        cell.textView.textColor = (message.senderId == senderId) ? outgoingMessageTextColor : incomingMessageTextColor
+        return cell
+    }
+}
