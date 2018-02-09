511e1df47e9950386c855a391ad945fbc8ebdf28
Arun Sivakumar <arunsivakumar@live.com>
2018-02-09 17:03:06 +0800

    gitignore credentials

---
+++ CirclesBot/Credentials.swift
@@ -0,0 +1,19 @@
+//
+//  Credentials.swift
+//  CirclesBot
+//
+//  Created by Lakshmi on 2/9/18.
+//  Copyright © 2018 com.arunsivakumar. All rights reserved.
+//
+
+import Foundation
+
+
+struct Credentials{
+    
+    
+    static let headerSubscriptionKey = "Ocp-Apim-Subscription-Key"
+    static let headerSubscriptionValue = "a74ef73882bc4069b20d7763be26c8b6"
+    
+    static let qnaLink = "/knowledgebases/e9126937-83ed-4adf-bb61-17e642b1748e/generateAnswer"
+}
